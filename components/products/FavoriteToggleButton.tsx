63ab876e85105bed789cb6a1a04d8f0f016240b9
James Lo <jameslo599@gmail.com>
2024-11-28 19:46:49 -0600

    no unused vars

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import { FaHeart } from 'react-icons/fa';
 
 const FavoriteToggleButton = ({ productId }: { productId: string }) => {
+  console.log(productId);
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />
